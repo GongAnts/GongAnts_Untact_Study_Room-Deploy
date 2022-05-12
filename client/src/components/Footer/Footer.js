75617389acd32176f8e26cea87b51a522d0f1b91
Seulg2027 <seulgi2027@dgu.ac.kr>
2022-05-12 18:53:25 +0900

    🎨[Design] Admin, Navbar, footer UI 변경

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,56 +1,14 @@
-/*!
-
-=========================================================
-* Light Bootstrap Dashboard React - v2.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-import React, { Component } from "react";
-import { Container } from "react-bootstrap";
+import React, { Component } from 'react';
 
 class Footer extends Component {
   render() {
     return (
-      <footer className="footer px-0 px-lg-3">
-        <Container fluid>
-          <nav>
-            <ul className="footer-menu">
-              <li>
-                <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                  Company
-                </a>
-              </li>
-              <li>
-                <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                  Portfolio
-                </a>
-              </li>
-              <li>
-                <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                  Blog
-                </a>
-              </li>
-            </ul>
-            <p className="copyright text-center">
-              © {new Date().getFullYear()}{" "}
-            </p>
-          </nav>
-        </Container>
+      <footer className="px-0 px-lg-3 bg-base-200 w-full h-20 pt-6 fixed bottom-0">
+        <nav>
+          <p className="copyright text-center">
+            HANIUM-GONGANTS © 2022 COPYRIGHT
+          </p>
+        </nav>
       </footer>
     );
   }
