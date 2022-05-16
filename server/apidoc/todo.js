223f222950a7d3ec766d679b03052e1b7c5af8ea
sugyeong kim <1466su@naver.com>
2022-05-17 03:57:23 +0900

    Squash For Release : v1.0.4-alpha

---
+++ server/apidoc/todo.js
@@ -0,0 +1,187 @@
+/**
+ * @api {get} /todo/all 전체 할일 로드
+ *
+ * @apiVersion 1.0.0
+ * @apiName 전체 할일 로드
+ * @apiGroup Todo
+ * @apiDescription 사용자가 작성한 모든 할일들을 로드합니다.(로그인 상태에서만 가능합니다.)
+ * @apiSuccess {String} user_id 유저 아이디
+ * @apiSuccess {Int} todo_id 할일 아이디
+ * @apiSuccess {String} todo_title 할일 내용
+ * @apiSuccess {Boolean} todo_check 완료 여부
+ * @apiSuccess {Datetime} create_date 생성 날짜
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     [
+ *         {
+ *             "user_id": "utest",
+ *             "todo_id": 1,
+ *             "todo_title": "할일 이름",
+ *             "todo_check": 0,
+ *             "create_date": "2022-04-20T05:46:36.000Z"
+ *         },
+ *         {
+ *             "user_id": "utest",
+ *             "todo_id": 2,
+ *             "todo_title": "할일 테스트",
+ *             "todo_check": 0,
+ *             "create_date": "2022-05-20T06:39:14.000Z"
+ *         }
+ *     ]
+ */
+
+/**
+ * @api {get} /todo/monthly 월별 할일 로드
+ *
+ * @apiVersion 1.0.0
+ * @apiName 선택된 월별 할일 로드
+ * @apiGroup Todo
+ * @apiDescription 선택된 월에 대한 할일들을 로드합니다.(로그인 상태에서만 가능합니다.)
+ * @apiParam {String} year 년
+ * @apiParam {String} month 월
+ * @apiSuccess {String} user_id 유저 아이디
+ * @apiSuccess {Int} todo_id 할일 아이디
+ * @apiSuccess {String} todo_title 할일 내용
+ * @apiSuccess {Boolean} todo_check 완료 여부
+ * @apiSuccess {Datetime} create_date 생성 날짜
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     [
+ *         {
+ *             "user_id": "utest",
+ *             "todo_id": 1,
+ *             "todo_title": "할일 테스트1",
+ *             "todo_check": 0,
+ *             "create_date": "2022-05-18T05:46:36.000Z"
+ *         },
+ *         {
+ *             "user_id": "utest",
+ *             "todo_id": 2,
+ *             "todo_title": "할일 테스트2",
+ *             "todo_check": 0,
+ *             "create_date": "2022-05-20T06:39:14.000Z"
+ *         }
+ *     ]
+ */
+
+/**
+ * @api {get} /todo/today 오늘 할일 로드
+ *
+ * @apiVersion 1.0.0
+ * @apiName 오늘 할일 로드
+ * @apiGroup Todo
+ * @apiDescription 오늘 할일들을 로드합니다.(로그인 상태에서만 가능합니다.)
+ * @apiSuccess {String} user_id 유저 아이디
+ * @apiSuccess {Int} todo_id 할일 아이디
+ * @apiSuccess {String} todo_title 할일 내용
+ * @apiSuccess {Boolean} todo_check 완료 여부
+ * @apiSuccess {Datetime} create_date 생성 날짜
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     [
+ *         {
+ *             "user_id": "utest",
+ *             "todo_id": 1,
+ *             "todo_title": "할일 테스트",
+ *             "todo_check": 0,
+ *             "create_date": "2022-05-18T05:46:36.000Z"
+ *         }
+ *     ]
+ */
+
+/**
+ * @api {get} /todo/detail 할일 상세 정보 로드
+ *
+ * @apiVersion 1.0.0
+ * @apiName 할일 상세 정보 로드
+ * @apiGroup Todo
+ * @apiDescription 할일 하나에 대한 정보를 로드합니다.(로그인 상태에서만 가능합니다.)
+ * @apiParam {String} id 할일 아이디
+ * @apiSuccess {String} user_id 유저 아이디
+ * @apiSuccess {Int} todo_id 할일 아이디
+ * @apiSuccess {String} todo_title 할일 내용
+ * @apiSuccess {Boolean} todo_check 완료 여부
+ * @apiSuccess {Datetime} create_date 생성 날짜
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     [
+ *         {
+ *             "user_id": "utest",
+ *             "todo_id": 1,
+ *             "todo_title": "할일 테스트",
+ *             "todo_check": 0,
+ *             "create_date": "2022-05-18T05:46:36.000Z"
+ *         }
+ *     ]
+ */
+
+/**
+ * @api {post} /todo 할일 추가
+ * @apiVersion 1.0.0
+ * @apiName 할일 추가
+ * @apiGroup Todo
+ * @apiDescription 새로운 할일을 추가합니다. (로그인 상태에서만 가능합니다.)
+ * @apiBody {String} todo_title 할일 내용
+ * @apiSuccess {String} user_id 유저 아이디
+ * @apiSuccess {Int} todo_id 할일 아이디
+ * @apiSuccess {String} todo_title 할일 내용
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *         "user_id": "utest",
+ *         "todo_title": "할일 추가 테스트"
+ *     }
+ */
+
+/**
+ * @api {put} /todo 할일 내용 수정
+ * @apiVersion 1.0.0
+ * @apiName 일정 수정
+ * @apiGroup Todo
+ * @apiDescription 할일 내용을 수정합니다. (로그인 상태에서만 가능합니다.)
+ * @apiBody {String} todo_id 할일 아이디
+ * @apiBody {String} todo_title 수정할 할일 내용
+ * @apiSuccess {Int} todo_id 할일 아이디
+ * @apiSuccess {String} todo_title 할일 내용
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *         "todo_id": 1,
+ *         "todo_title": "할일 수정 테스트"
+ *     }
+ */
+
+/**
+ * @api {put} /todo/check 할일 상태 수정
+ * @apiVersion 1.0.0
+ * @apiName 할일 상태 수정
+ * @apiGroup Todo
+ * @apiDescription 할일의 완료 여부를 수정합니다. - {"todo_check" : 0} -> 미완료 된 할일 / {"todo_check" : 1} -> 완료 된 할일
+ * @apiParam {String} id 할일 아이디
+ * @apiSuccess {Int} todo_id 할일 아이디
+ * @apiSuccess {String} todo_title 할일 내용
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *          "todo_id": "1"
+ *     }
+ */
+
+/**
+ * @api {delete} /todo 할일 삭제
+ * @apiVersion 1.0.0
+ * @apiName 할일 삭제
+ * @apiGroup Todo
+ * @apiDescription 기존 할일을 삭제합니다.
+ * @apiParam {String} id 할일 아이디
+ * @apiSuccess {Int} todo_id 할일 아이디
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *          "todo_id": "1"
+ *     }
+ */
--- server/routes/todo.js
+++ server/routes/todo.js
@@ -5,23 +5,21 @@ const db = require('../config/db');
 router.use(express.json());
 
 // 전체 할일 리스트 로드
-router.get('/load/all', (req, res) => {
+router.get('/all', (req, res) => {
   var user_id = req.user.user_id;
 
   const sql = `SELECT * FROM todo WHERE user_id = '${user_id}'`;
   db.query(sql, (err, data) => {
     if (!err) {
-      console.log('전체 할일 리스트 조회 성공', data);
       res.status(200).send(data);
     } else {
-      console.log('전체 할일 리스트 조회 실패');
       res.status(500).send(err);
     }
   });
 });
 
 //월별 할일 리스트 로드
-router.get('/load/monthly', (req, res) => {
+router.get('/monthly', (req, res) => {
   var year = req.query.year;
   var month = req.query.month;
   var user_id = req.user.user_id;
@@ -33,40 +31,43 @@ router.get('/load/monthly', (req, res) => {
     AND DATE_FORMAT(create_date, '%Y-%c') = '${year}-${month}'`;
   db.query(sql, (err, data) => {
     if (!err) {
-      console.log('월별 할일 리스트 조회 성공', data);
       res.status(200).send(data);
     } else {
-      console.log('월별 할일 리스트 조회 실패');
       res.status(500).send(err);
     }
   });
 });
 
 // 오늘 할일 리스트 로드
-router.get('/load/today', (req, res) => {
+router.get('/today', (req, res) => {
   var user_id = req.user.user_id;
 
   const sql = `SELECT * FROM todo WHERE user_id = '${user_id}'\
     AND DATE(create_date) = DATE(now())`;
   db.query(sql, (err, data) => {
     if (!err) {
-      // 기존 저장 데이터가 있을 경우 로드
-      if (data.length) {
-        console.log('오늘 할일 리스트 로딩 성공');
-        res.status(200).send(data);
-      } else {
-        console.log('오늘 할일 리스트 미존재');
-        res.status(200).send({ msg: 'Nothing' });
-      }
+      res.status(200).send(data);
     } else {
-      console.log('오늘 할일 리스트 로딩 실패');
       res.status(500).send(err);
     }
   });
 });
 
-// 할일 저장
-router.post('/write', (req, res) => {
+// 할일 상세 정보 로드
+router.get('/detail', (req, res) => {
+  const todo_id = req.query.id;
+  const sql = `SELECT * FROM todo WHERE todo_id = '${todo_id}'`;
+  db.query(sql, (err, data) => {
+    if (!err) {
+      res.status(200).send(data);
+    } else {
+      res.status(500).send(err);
+    }
+  });
+});
+
+// 할일 추가
+router.post('/', (req, res) => {
   var user_id = req.user.user_id;
   const todo_title = req.body.todo_title;
 
@@ -74,7 +75,6 @@ router.post('/write', (req, res) => {
     user_id: `${user_id}`,
     todo_title: `${todo_title}`,
   };
-  console.log(todo_data);
   if (!todo_title) {
     return res.status(400).send({ statusMsg: 'Bad Request' });
   }
@@ -82,54 +82,15 @@ router.post('/write', (req, res) => {
 
   db.query(sql, (err, data) => {
     if (!err) {
-      console.log('할일 저장 성공');
       res.status(200).send(todo_data);
     } else {
-      console.log('할일 저장 실패');
-      res.status(500).send(err);
-    }
-  });
-});
-
-// request URL Example : http://localhost:4000/todo/detail?id=1
-router.get('/detail', (req, res) => {
-  const todo_id = req.query.id;
-  const sql = `SELECT * FROM todo WHERE todo_id = '${todo_id}'`;
-  db.query(sql, (err, data) => {
-    if (!err) {
-      console.log('할일 로딩 성공');
-      res.status(200).send(data);
-    } else {
-      console.log('할일 로딩 실패');
-      res.status(500).send(err);
-    }
-  });
-});
-
-// 할일 삭제
-router.delete('/delete', (req, res, next) => {
-  const todo_id = req.query.id;
-  var todo_data = {
-    todo_id: `${todo_id}`,
-  };
-  console.log(todo_data);
-  if (!todo_id) {
-    return res.status(400).send({ statusMsg: 'Bad Request' });
-  }
-  const sql = `DELETE FROM todo WHERE todo_id = ${todo_id};`;
-  db.query(sql, (err, data) => {
-    if (!err) {
-      console.log('할일 삭제 성공');
-      res.status(200).send(todo_data);
-    } else {
-      console.log('할일 삭제 실패');
       res.status(500).send(err);
     }
   });
 });
 
 // 할일 수정
-router.post('/modify', (req, res) => {
+router.put('/', (req, res) => {
   const { todo_id, todo_title } = req.body;
   if (!todo_id || !todo_title) {
     return res.status(400).send({ statusMsg: 'Bad Request' });
@@ -137,28 +98,46 @@ router.post('/modify', (req, res) => {
   const sql = `UPDATE todo SET todo_title = '${todo_title}' WHERE todo_id = '${todo_id}';`;
   db.query(sql, (err, data) => {
     if (!err) {
-      console.log('할일 수정 성공');
       res.status(200).send(req.body);
     } else {
-      console.log('할일 수정 실패');
       res.status(500).send(err);
     }
   });
 });
 
 // 할일 상태 수정
-router.post('/check', (req, res) => {
-  const todo_id = req.body.todo_id;
+router.put('/check', (req, res) => {
+  const todo_id = req.query.id;
+  var todo_data = {
+    todo_id: `${todo_id}`,
+  };
   if (!todo_id) {
     return res.status(400).send({ statusMsg: 'Bad Request' });
   }
   const sql = `UPDATE todo SET todo_check = !todo_check WHERE todo_id = ${todo_id};`;
   db.query(sql, (err, data) => {
     if (!err) {
-      console.log('할일 상태 수정 성공');
-      res.status(200).send(req.body);
+      res.status(200).send(todo_data);
     } else {
-      console.log('할일 상태 수정 실패');
+      res.status(500).send(err);
+    }
+  });
+});
+
+// 할일 삭제
+router.delete('/', (req, res, next) => {
+  const todo_id = req.query.id;
+  var todo_data = {
+    todo_id: `${todo_id}`,
+  };
+  if (!todo_id) {
+    return res.status(400).send({ statusMsg: 'Bad Request' });
+  }
+  const sql = `DELETE FROM todo WHERE todo_id = ${todo_id};`;
+  db.query(sql, (err, data) => {
+    if (!err) {
+      res.status(200).send(todo_data);
+    } else {
       res.status(500).send(err);
     }
   });
